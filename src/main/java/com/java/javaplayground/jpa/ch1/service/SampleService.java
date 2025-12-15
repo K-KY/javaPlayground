7303fda141f3c8a9d4b12102edb8e19694ffd10d
K-KY <kyuyoungk@naver.com>
2025-12-16 03:15:36 +0900

    JPA persist Sample

---
+++ src/main/java/com/java/javaplayground/jpa/ch1/service/SampleService.java
@@ -0,0 +1,24 @@
+package com.java.javaplayground.jpa.ch1.service;
+
+import com.java.javaplayground.jpa.ch1.entity.Sample;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SampleService {
+    private final EntityManager entityManager;
+
+    public SampleService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    public void save() {
+        Sample sample = new Sample("saved" + (int)(Math.random() * 100), 100, "M");
+        entityManager.persist(sample);
+    }
+
+}
+
+
