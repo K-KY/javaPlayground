808981613e8191c0da7ec6dd2a6252fb6f622d6b
K-KY <kyuyoungk@naver.com>
2025-12-16 03:14:01 +0900

    JPA persist Sample

---
+++ src/main/java/com/java/javaplayground/jpa/ch1/entity/Sample.java
@@ -0,0 +1,26 @@
+package com.java.javaplayground.jpa.ch1.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Sample {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public Sample(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Sample() {
+    }
+}
